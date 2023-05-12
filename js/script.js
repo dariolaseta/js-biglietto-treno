3a219aaf00b9065652ced0ba8e4d352b835b7229
dariolaseta <dario.laseta@gmail.com>
2023-05-12 15:57:21 +0200

    added toFixed method & variable for final price

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ const userAge = parseInt(prompt("Quanti anni hai?"));
 const kmPrice = 0.21;
 const normalTicketPrice = km * kmPrice;
 let discountPrice;
+let finalPrice;
 
 if(userAge < 18){
     discountPrice = (normalTicketPrice / 100) * 20;
-    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + (normalTicketPrice - discountPrice) + "&euro;";
+    finalPrice = normalTicketPrice - discountPrice;
+    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + finalPrice.toFixed(2) + "&euro;";
 }else if(userAge > 65){
     discountPrice = (normalTicketPrice / 100) * 40;
-    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + (normalTicketPrice - discountPrice) + "&euro;";
+    finalPrice = normalTicketPrice - discountPrice;
+    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + finalPrice.toFixed(2) + "&euro;";
 }else{
     document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + normalTicketPrice + "&euro;";
 }
