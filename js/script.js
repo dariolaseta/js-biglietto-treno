5be2294ce6095c3e9f98cc9759f4520c6a3155ad
dariolaseta <dario.laseta@gmail.com>
2023-05-12 15:43:03 +0200

    added normal ticket price variable

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 const userAge = parseInt(prompt("Quanti anni hai?"));
 const kmPrice = 0.21;
+const normalTicketPrice = km * kmPrice;
