f75e32e3188aa70965b5412d8f6cc3c42c090603
dariolaseta <dario.laseta@gmail.com>
2023-05-12 15:54:46 +0200

    added discounts

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 const userAge = parseInt(prompt("Quanti anni hai?"));
 const kmPrice = 0.21;
 const normalTicketPrice = km * kmPrice;
+let discountPrice;
 
 if(userAge < 18){
-    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + ((normalTicketPrice / 100) * 20) + "&euro;";
+    discountPrice = (normalTicketPrice / 100) * 20;
+    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + (normalTicketPrice - discountPrice) + "&euro;";
+}else if(userAge > 65){
+    discountPrice = (normalTicketPrice / 100) * 40;
+    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + (normalTicketPrice - discountPrice) + "&euro;";
+}else{
+    document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + normalTicketPrice + "&euro;";
 }
